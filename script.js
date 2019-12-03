712f7a4439e2252938499549b16c4c06a4b120e8
LaHesis <cryptograph2013@yandex.ru>
2019-12-03 15:45:57 +0700

    addition of revealing safe cells and cells around safe cells

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const DEBUG = false;
+
 function Cell(x, y, hasMine = false, minesAroundAmount = 0) {
     this.x = x;
     this.y = y;
@@ -5,21 +7,42 @@ function Cell(x, y, hasMine = false, minesAroundAmount = 0) {
     this.minesAroundAmount = minesAroundAmount;
 }
 
-
-
 function Model(playFieldWidth, playFieldHeight, minesAmount) {
     this.playFieldWidth = (playFieldWidth >= 10) ? playFieldWidth : 10;
     this.playFieldHeight = (playFieldHeight >= 10) ? playFieldHeight : 10;
-    this.minesAmount = minesAmount;
+    this.minesAmount = (minesAmount >= 5) ? minesAmount : 5;
 
     this.mineAllocationTryLimit = 4000;
     this.field = [];
+    this.minedCells = [];
+    this.openedCellsAmount = 0;
+    this.maxNotMinedOpenedCellsAmount = this.playFieldHeight * this.playFieldWidth - minesAmount;
+    this.gameIsOver = false;
+    this.debug = true;
+
+    this.getCellsAroundCell = (cell) => {
+        const y = cell.y;
+        const x = cell.x;
+        let cellsAround = [];
+        for (let upperCellsX = x - 1; upperCellsX < x + 2; upperCellsX++) {
+            const upperCell = this.field[y - 1] ? this.field[y - 1][upperCellsX] : null
+            cellsAround.push(upperCell);
+        }
+        for (let lowerCellsX = x - 1; lowerCellsX < x + 2; lowerCellsX++) {
+            const lowerCell = this.field[y + 1] ? this.field[y + 1][lowerCellsX] : null;
+            cellsAround.push(lowerCell);
+        }
+        const leftCell = this.field[y][x - 1];
+        const rightCell = this.field[y][x + 1];
+        cellsAround.push(leftCell, rightCell)
+        cellsAround = cellsAround.filter(cell => cell != undefined)
+        return cellsAround;
+    }
 
     this.placeMines = () => {
-        let mines = [];
         let tryCount = 0;
         const field = this.field;
-        while (mines.length < this.minesAmount) {
+        while (this.minedCells.length < this.minesAmount) {
             if (tryCount > this.mineAllocationTryLimit) {
                 alert('Too many mines! Try to decrease the amount)');
                 break;
@@ -29,71 +52,20 @@ function Model(playFieldWidth, playFieldHeight, minesAmount) {
             let theCell = field[y][x];
             if (theCell.hasMine == false) {
                 theCell.hasMine = true;
-                mines.push(theCell);
+                this.minedCells.push(theCell);
             }
             tryCount++;
         }
 
         // minesAroundAmount computing.
-        const aroundLeftTop = [
-            field[0][1],
-            field[1][0],
-            field[1][1]
-        ];
-        // Test
-        // aroundLeftTop.forEach((elem) => elem.hasMine = true);
-        field[0][0].minesAroundAmount = aroundLeftTop.filter((el) => el.hasMine).length;
-
-        const aroundRightTop = [
-            field[0][field[0].length - 2],
-            field[1][field[0].length - 2],
-            field[1][field[0].length - 1]
-        ]
-        // Test.
-        // aroundRightTop.forEach((elem) => elem.hasMine = true);
-        field[0][field[0].length - 1].minesAroundAmount = aroundRightTop.filter((el) => el.hasMine).length;
-
-        const aroundLeftBottom = [
-            field[field.length - 2][0],
-            field[field.length - 2][1],
-            field[field.length - 1][1]
-        ]
-        // Test.
-        // aroundLeftBottom.forEach((elem) => elem.hasMine = true);
-        field[field.length - 1][0].minesAroundAmount = aroundLeftBottom.filter((el) => el.hasMine).length;
-
-        const aroundRightBottom = [
-            field[field.length - 2][field[0].length - 1],
-            field[field.length - 2][field[0].length - 2],
-            field[field.length - 1][field[0].length - 2],
-        ]
-        // Test.
-        // aroundRightBottom.forEach((elem) => elem.hasMine = true);
-        field[field.length - 1][field[0].length - 1].minesAroundAmount = aroundRightBottom.filter((el) => el.hasMine).length;
-
         for (let y = 0; y < this.playFieldHeight; y++) {
             for (let x = 0; x < this.playFieldWidth; x++) {
-                for (let upperCellsX = x - 1; upperCellsX < x + 2; upperCellsX++) {
-                    const upperCell = field[y - 1] ? field[y - 1][upperCellsX] : null
-                    if (upperCell != undefined && field[y - 1][upperCellsX].hasMine)
-                        field[y][x].minesAroundAmount++;
-                }
-                for (let lowerCellsX = x - 1; lowerCellsX < x + 2; lowerCellsX++) {
-                    const lowerCell = field[y + 1] ? field[y + 1][lowerCellsX] : null;
-                    if (lowerCell && field[y + 1][lowerCellsX].hasMine)
-                        field[y][x].minesAroundAmount++;
-                }
-                const leftCell = field[y][x - 1];
-                if (leftCell != undefined && leftCell.hasMine)
-                    field[y][x].minesAroundAmount++;
-                const rightCell = field[y][x + 1];
-                if (rightCell != undefined && rightCell.hasMine)
-                    field[y][x].minesAroundAmount++;
+                this.getCellsAroundCell(field[y][x])
+                    .filter(cell => cell.hasMine)
+                    .forEach(cell => field[y][x].minesAroundAmount++);
             }
         }
     }
-
-    // this.putMinesAroundAmount =
 
     this.fillField = () => {
         // Cell objects creation.
@@ -110,19 +82,82 @@ function Model(playFieldWidth, playFieldHeight, minesAmount) {
 
 
 function Controller() {
-    model = new Model(15, 15, 50);
-    // model = new Model(Number(prompt('Enter field of play width (10 default): ', '10')),
-    //             Number(prompt('Enter field of play height (10 default): ', '10')),
-    //             Number(prompt('Enter mine amount (10 default): ', '10')));
+    if (DEBUG)
+        model = new Model(15, 15, 5);
+    else
+        model = new Model(Number(prompt('Enter field of play width (the minimum is 10): ', '10')),
+                    Number(prompt('Enter field of play height (the minimum is 10): ', '10')),
+                    Number(prompt('Enter mine amount (the minimum is 5): ', '10')));
     model.fillField();
     model.placeMines();
     view = new View(model.field);
     view.stylizeAndScaleCells()
 
+    function openACell(cell, model) {
+        if (!model.gameIsOver) {
+            if (!cell.hasMine) model.openedCellsAmount++;
+            if (model.openedCellsAmount == model.maxNotMinedOpenedCellsAmount) {
+                handleAWin(model);
+            }
+            cell.tag.classList.remove('cell-covered');
+            cell.tag.classList.add('cell-revealed');
+            if (cell.hasMine) {
+                cell.tag.classList.add('cell-mined');
+                handleALoss(model);
+            }
+            else if (cell.minesAroundAmount == 0) {
+                const cellRevealedBgColor = 'rgb(14, 105, 93)'
+                cell.tag.style.color = cellRevealedBgColor;
+                return 'safeCell'
+            }
+        }
+    }
+
     // Cell click handling.
     for (let y = 0; y < model.field.length; y++) {
         model.field[y].forEach(cell => {
             cell.tag.addEventListener('click', (event) => {
+                let cellsToReveal = [cell]
+                do {
+                    // Because some cells can already be opened by previous loop iteration.
+                    cellsToReveal = cellsToReveal.filter(cell =>
+                        cell.tag.classList.contains('cell-covered'))
+                    if (cellsToReveal.length) {
+                        const cellToReveal = cellsToReveal.pop(0);
+                        const openingResult = openACell(cellToReveal, model);
+                        if (openingResult == 'safeCell') {
+                            const cellsAround = model.getCellsAroundCell(cellToReveal).filter(cell =>
+                                cell.tag.classList.contains('cell-covered'));
+                            cellsToReveal = cellsToReveal.concat(cellsAround);
+                        }
+                    }
+                } while (cellsToReveal.length);
+            });
+        });
+    }
+
+    function handleAWin(model) {
+        model.gameIsOver = true;
+        model.minedCells.forEach(cell => {
+            cell.tag.innerText = view.WinMineCharacter;
+            cell.tag.style.backgroundColor = '#0d0';
+            cell.tag.style.fontWeight = 'bold';
+        });
+        revealAllCells(model);
+        alert('You won!!! Congratulations!');
+    }
+
+    function handleALoss(model) {
+        if (!DEBUG) {
+            model.gameIsOver = true;
+            revealAllCells(model);
+            alert('Sorry, but you lost(. You can try one more time if you wish and have time.');
+        }
+    }
+
+    function revealAllCells(model) {
+        for (let y = 0; y < model.field.length; y++) {
+            model.field[y].forEach(cell => {
                 cell.tag.classList.remove('cell-covered');
                 cell.tag.classList.add('cell-revealed');
                 if (cell.hasMine)
@@ -130,28 +165,22 @@ function Controller() {
                 else if (cell.minesAroundAmount == 0)
                     cell.tag.style.color = 'rgb(14, 105, 93)';
             });
-        });
+        }
     }
 
     // For debug to open all cells.
-    document.body.addEventListener('keydown', (event) => {
-        if (event.code == 'Space') {
-            for (let y = 0; y < model.field.length; y++) {
-                    model.field[y].forEach(cell => {
-                        cell.tag.classList.remove('cell-covered');
-                        cell.tag.classList.add('cell-revealed');
-                        if (cell.hasMine)
-                            cell.tag.classList.add('cell-mined');
-                        else if (cell.minesAroundAmount == 0)
-                            cell.tag.style.color = 'rgb(14, 105, 93)';
-                    });
+    if (DEBUG)
+        document.body.addEventListener('keydown', (event) => {
+            if (event.code == 'Space') {
+                revealAllCells(model);
             }
-        }
-    });
+        });
 }
 
 function View(field) {
     this.fieldTag = document.getElementById('field');
+    const mineCharacter = '😢'
+    this.WinMineCharacter = '✔'
     this.theLongestFieldSide = field[0].length > field.length ? [field[0].length, 'wIsLonger'] : [field.length, 'hIsLonger'];
     if (field[0].length == field.length)
         this.theLongestFieldSide[1] = 'equal';
@@ -188,7 +217,7 @@ function View(field) {
             let cellTag = document.createElement('div');
             cellTag.classList.add('cell', 'cell-covered');
             if (cell.hasMine) {
-                cellTag.appendChild(document.createTextNode('😢'));
+                cellTag.appendChild(document.createTextNode(mineCharacter));
             }
             else
                 cellTag.appendChild(document.createTextNode(cell.minesAroundAmount));
@@ -207,10 +236,10 @@ function View(field) {
                 field[y][x].tag.style.fontSize = cellSize + 'px';
                 field[y][x].tag.style.transitionDelay = (y / 4 * this.theLongestFieldSide[0] + x) / 40 + 's';
                 field[y][x].tag.classList.add('cell-appeared');
-
             }
         }
         // Reset transition delay.
+        const initialAnimationDelay = 3;
         setTimeout(() => {
         for (let y = 0; y < field.length; y++) {
             for (let x = 0; x < field[y].length; x++) {
@@ -218,7 +247,7 @@ function View(field) {
                 field[y][x].tag.style.transitionDuration = '.15s';
             }
         }
-        }, (3));
+        }, (initialAnimationDelay));
     }
 }
 
